4a640f97178950e1610961e9a181457bc5e03357
alexandr <k4t4kun@gmail.com>
2022-02-20 17:44:14 +0900

    9.1 Зависимость от System.out [#33568] Исправление - добавил out.

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -30,6 +30,11 @@ public class StartUI {
         Tracker tracker = new Tracker();
         UserAction[] actions = {
                 new CreateAction(output),
+                new ShowAllAction(output),
+                new ReplaceAction(output),
+                new DeleteAction(output),
+                new FindItemByIdAction(output),
+                new FindItemByNameAction(output),
                 new Exit()
         };
         new StartUI(output).init(input, tracker, actions);
