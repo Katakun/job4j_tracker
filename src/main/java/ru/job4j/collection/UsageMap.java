e39f3a676b5bf8e07b29e846d76301010b28edce
alexandr <k4t4kun@gmail.com>
2022-05-16 13:57:54 +0900

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("pochta@mail.ru", "Ivan Ivanov");
+        map.put("vasya@mail.ru", "Vasiliy Petrov");
+        map.put("masha@mail.ru", "Masha Sidorova");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key  + " - " + value);
+        }
+    }
+}
